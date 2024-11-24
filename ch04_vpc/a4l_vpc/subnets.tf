d4442aaed489791054dfcd1e124a657e261ba7bd
brencoder <6211560+brencoder@users.noreply.github.com>
2024-11-24 16:05:29 +1100

    Create all 12 A4L subnets

--- ch04_vpc/a4l_vpc/subnets.tf
+++ ch04_vpc/a4l_vpc/subnets.tf
@@ -1,9 +1,10 @@
 resource "aws_subnet" "a4l_vpc1_subnets" {
   for_each = var.a4l_vpc1_subnet_details
 
-  availability_zone = each.value.az
-  cidr_block        = each.value.ipv4_cidr
-  ipv6_cidr_block   = cidrsubnet(aws_vpc.a4l_vpc1.ipv6_cidr_block, var.a4l_vpc1_ipv6_netbits, each.value.ipv6_netnum)
+  assign_ipv6_address_on_creation = true
+  availability_zone               = each.value.az
+  cidr_block                      = each.value.ipv4_cidr
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.a4l_vpc1.ipv6_cidr_block, var.a4l_vpc1_ipv6_netbits, each.value.ipv6_netnum)
   tags = {
     Brendon = "",
     Name    = each.value.name
